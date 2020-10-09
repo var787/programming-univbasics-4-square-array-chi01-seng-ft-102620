b292de77b0dee14ad8c68ac466e8f4fa1f89fa88
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-09 06:38:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array=[]
   counter=0
-  while counter>array.length
+  while counter<array.length
     new_array.push(array[counter]*array[counter])
     counter+=1
 end
